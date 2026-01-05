ea6fd4d4ef6cf3c0baf9a00c6f057d563a3390df
JGARESC <jgaresc@alumno.upv.es@ldsic-vdi06.upvnet.upv.es>
2026-01-05 16:06:36 +0100

    Actualizado Makefile y añadido código de test de la clase BSTreeDict

--- TableEntry.h
+++ TableEntry.h
@@ -6,35 +6,35 @@
 
 template <typename V>
 class TableEntry {
-public:
-    std::string key;
-    V value;
+    public:
+        std::string key;
+        V value;
 
-    TableEntry(std::string key, V value) {
-        this->key = key;
-        this->value = value;
-    }
+        TableEntry(std::string key, V value) : key(key), value(value) {}
+        TableEntry(std::string key) : key(key), value(V()) {}
+        TableEntry() : key(""), value(V()) {}
 
-    TableEntry(std::string key) {
-        this->key = key;
-    }
+        friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+            return te1.key == te2.key;
+        }
 
-    TableEntry() {
-        this->key = "";
-    }
+        friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+            return te1.key != te2.key;
+        }
 
-    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
-        return te1.key == te2.key;
-    }
+        // NUEVOS OPERADORES PARA BSTree
+        friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+            return te1.key < te2.key;
+        }
 
-    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
-        return te1.key != te2.key;
-    }
+        friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+            return te1.key > te2.key;
+        }
 
-    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
-        out << te.key << " -> " << te.value;
-        return out;
-    }
+        friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
+            out << te.key << " -> " << te.value;
+            return out;
+        }
 };
 
 #endif
